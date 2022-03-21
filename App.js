fcf49c188d7b9d4edca9de394bce77f4f8dea8fc
Sibusiso9323 <sibusisosiyanda5@gmail.com>
2022-03-21 17:10:52 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import searchUser from './components/searchUser';
+import ResultsNotFound from './components/ResultsNotFound';
+import Repos from './components/repos';
+
+
+const Stack = createNativeStackNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="searchUser" component={searchUser} options={{headerShown:false}}/>
+        <Stack.Screen name="Repos" component={Repos} />
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
